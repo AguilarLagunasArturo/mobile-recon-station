31bc9a78deb6cdf132ee132210ae02572de3c0be
AguilarLagunasArturo <aguilar.lagunas.arturo@gmail.com>
2021-06-05 20:59:17 -0500

    Website update

--- logic.js
+++
@@ -1,8 +0,0 @@
-function send(msg){
-  var url = 'localhost/#state';
-  var xhr = new XMLHttpRequest();
-  xhr.open('POST', url, true);
-  xhr.setRequestHeader("Content-Type", "application/mobile-recon-station; charset=UTF-8");
-  xhr.send('param1='+msg);
-  console.log(msg);
-}
