fca0481634b21cdb06934e142e3afa98b694b81f
AguilarLagunasArturo <aguilar.lagunas.arturo@gmail.com>
2021-06-05 22:25:03 -0500

    Website update

--- sketch.js
+++ sketch.js
@@ -1,3 +1,8 @@
+var x = 0;
+var y = 0;
+var z = 0;
+var step = 10;
+
 function setup() {
 	var c = createCanvas(
 		screen.availWidth * (1-0.2),
@@ -9,19 +14,24 @@ function setup() {
 
 function draw() {
 	background(100, 200, 200);
-	rotateX(frameCount * 0.01);
-	rotateY(frameCount * 0.01);
-	box(50);
+	// rotateX(frameCount * 0.005);
+	// rotateY(frameCount * 0.005);
+	translate(x, y, z);
+	box(80, 50, 100);
 }
 
 function keyPressed() {
 	if (keyCode === UP_ARROW) {
+		z -= step;
 		console.log("up");
 	} else if (keyCode === DOWN_ARROW) {
+		z += step;
 		console.log("down");
 	} else if (keyCode === RIGHT_ARROW) {
+		x += step;
 		console.log("right");
 	} else if (keyCode === LEFT_ARROW) {
+		x -= step;
 		console.log("left");
 	}
 }
