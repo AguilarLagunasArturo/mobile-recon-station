4de3897005ed8da467bcd0bddefd13b21e728744
AguilarLagunasArturo <aguilar.lagunas.arturo@gmail.com>
2021-06-12 20:15:24 -0500

    Working on gyro

--- car/mobile/Gyro.py
+++ car/mobile/Gyro.py
@@ -4,32 +4,62 @@ Datasheet:		https://43zrtwysvxb2gf29r5o0athu-wpengine.netdna-ssl.com/wp-content/
 '''
 
 import smbus
-
-''' CONSTANTS '''
-ACC_SCALE_2G = 16384.0
-GYRO_SCALE_250D = 250.0
+import numpy as np
 
 ''' REGISTERS [links] '''
 ADRS = 0x68
 
-ACC_CONF = 0x1C
 GYRO_CONF = 0x1B
+ACC_CONF = 0x1C
 
-ACCEL_RANGE_2G = 0x00
-GYRO_RANGE_250DEG = 0x00
+GYRO_RANGES = [
+	0x00,		# +/- 250  DEG/s
+	0x01,		# +/- 500  DEG/s
+	0x02,		# +/- 1000 DEG/s
+	0x03		# +/- 2000 DEG/s
+]
 
+GYRO_SCALE = [
+	131.0,		# LSB / (DEG/s)
+	65.5,		# LSB / (DEG/s)
+	32.8,		# LSB / (DEG/s)
+	16.4		# LSB / (DEG/s)
+]
+
+ACC_RANGES = [
+	0x00,		# +/- 2  g
+	0x01,		# +/- 4  g
+	0x02,		# +/- 8  g
+	0x03		# +/- 16 g
+]
+
+ACC_SCALE = [
+	16384.0,	# LSB/g
+	8192.0,		# LSB/g
+	4096.0,		# LSB/g
+	2048.0		# LSB/g
+]
+
+ACC_X = 0x3B
 ACC_Y = 0x3D
+ACC_Z = 0x3F
+
+GYRO_X = 0x43
+GYRO_Y = 0x45
 GYRO_Z = 0x47
 
 PWR_MGMT_1 = 0x6B
 
-''' functions  '''
 bus = smbus.SMBus(1)
 
+''' functions  '''
+gyro_mode = 0
+acc_mode = 0
+
 def setup():
-	bus.write_byte_data(ADRS, PWR_MGMT_1, 0x08)
-	bus.write_byte_data(ADRS, ACC_CONF, ACCEL_RANGE_2G)
-	bus.write_byte_data(ADRS, GYRO_CONF, GYRO_RANGE_250DEG)
+	bus.write_byte_data(ADRS, PWR_MGMT_1, 0x08)						# TEMP OFF
+	bus.write_byte_data(ADRS, GYRO_CONF, GYRO_RANGES[gyro_mode])	# GYRO RANGE
+	bus.write_byte_data(ADRS, ACC_CONF, ACC_RANGES[acc_mode])		# GYRO ACC
 
 def read_address(address):
 	h = bus.read_byte_data(ADRS, address)
@@ -42,19 +72,42 @@ def read_address(address):
 	else:
 		return value
 
-def get_acc():
-	acc_y = read_address(ACC_Y)
-	ay = acc_y/ACC_SCALE_2G
-	return ay
-
 def get_gyro():
+	gyro_x = read_address(GYRO_X)
+	gyro_y = read_address(GYRO_Y)
 	gyro_z = read_address(GYRO_Z)
-	gz = gyro_z/GYRO_SCALE_250D
-	return gz
+
+	gx = gyro_x/GYRO_SCALE[gyro_mode]
+	gy = gyro_y/GYRO_SCALE[gyro_mode]
+	gz = gyro_z/GYRO_SCALE[gyro_mode]
+
+	return np.array([gx, gy, gz])
+
+def get_acc():
+	acc_x = read_address(ACC_X)
+	acc_y = read_address(ACC_Y)
+	acc_z = read_address(ACC_Z)
+
+	ax = acc_x/ACC_SCALE[acc_mode]
+	ay = acc_y/ACC_SCALE[acc_mode]
+	az = acc_z/ACC_SCALE[acc_mode]
+
+	return np.array([ax, ay, az])
+
+def calibrate(iterations):
+	a = np.array([0.0, 0.0, 0.0])
+	w = np.array([0.0, 0.0, 0.0])
+	for n in range(iterations):
+		a = a + get_acc()
+		w = w + get_gyro()
+	return [a/iterations, w/iterations]
 
 setup()
 
+a_off, w_off = calibrate(1000)
 while True:
-	a = get_acc()
-	w = get_gyro()
-	print(a, w)
+	a = get_acc() - a_off
+	w = get_gyro() - w_off
+	print('acc\t', a)
+	print('gyro\t', w)
+	print()
