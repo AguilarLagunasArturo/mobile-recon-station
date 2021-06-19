19f168b6c4087b0743b9f4c9631a547e1ac4e798
AguilarLagunasArturo <aguilar.lagunas.arturo@gmail.com>
2021-06-19 08:09:05 -0500

    Update

--- README.md
+++ README.md
@@ -4,9 +4,13 @@ An average car.
 # Dependencies
 | Name | Installation |
 | :------------- | :------------- |
-| Phyton3 | Official guide |
-| OpenCV >= 4 | Official guide |
+| Phyton3 | [Official website](2) |
+| OpenCV >= 4.5.2 | [Official guide](1) |
 | numpy | pip install numpy |
 | cv_recon | pip install cv-recon |
 | flask | pip install Flask |
 | smbus | sudo apt install python3-smbus |
+| matplotlib >= 3.3.4 | pip install matplotlib |
+
+[1]:https://docs.opencv.org/4.5.2/da/df6/tutorial_py_table_of_contents_setup.html
+[2]:https://www.python.org/downloads/
---
+++ car/data/graph_path.py
@@ -0,0 +1,39 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+import random
+
+if len(sys.argv) < 2:
+    raise Exception('Missing args')
+    exit()
+
+with open(sys.argv[1], 'r') as f:
+    lines = f.read().split('\n')
+
+values = [];
+for line in lines:
+    if line:
+        values.append( [val for val in line.split(',')] )
+
+values = np.longdouble(values)
+
+t = values[:, 0]
+dt = values[:, 1]
+wz = values[:, 7]
+
+theta = dt * wz
+theta = np.cumsum(theta)
+
+sx = dt * 1 * np.cos(theta * np.pi/180)
+sx = np.cumsum(sx)
+sy = dt * 1 * np.sin(theta * np.pi/180)
+sy = np.cumsum(sy)
+sz = [ np.sin(x * 1/120) for x in range( len(sy) )]
+
+ax = plt.figure().add_subplot(projection='3d')
+ax.plot(sx, sy, sz)
+ax.axes.set_zlim3d(bottom=-15, top=15)
+plt.show()
